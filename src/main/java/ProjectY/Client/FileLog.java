1f0a7c7542625d33d069c12c17c92411c1f7e2b8
orin_ <s0201603@ad.ua.ac.be>
2023-05-15 09:36:27 +0200

    Replication

--- src/main/java/ProjectY/Client/FileLog.java
+++ src/main/java/ProjectY/Client/FileLog.java
@@ -1,5 +1,8 @@
 package ProjectY.Client;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import java.util.Vector;
 
 public class FileLog {
@@ -7,7 +10,6 @@ public class FileLog {
         private int fileID;
         private int owner;
         private Vector<Integer> replicatedOwners;
-        private boolean replicated=false;
         private Vector<String> downloadLocations;
 
         public FileLog(String fileName, int fileID) {
@@ -15,5 +17,23 @@ public class FileLog {
             this.fileID = fileID;
         }
         public void setOwner(int ownerID) {this.owner = ownerID;}
+
+        public JSONObject toJSON(){
+            JSONObject response = new JSONObject();
+            response.put("fileName",fileName);
+            response.put("fileID",fileID);
+            response.put("owner",owner);
+            JSONArray replicatedOwnerJSON = new JSONArray();
+            for(int i = 0;i<replicatedOwners.size();i++){
+                replicatedOwnerJSON.add(i,replicatedOwners.get(i));
+            }
+            response.put("replicatedOwners",replicatedOwnerJSON);
+            JSONArray downloadLocationsJSON = new JSONArray();
+            for(int i = 0;i<downloadLocations.size();i++){
+                replicatedOwnerJSON.add(i,downloadLocations.get(i));
+            }
+            response.put("downloadLocation",downloadLocationsJSON);
+            return response;
+        }
 
     }
