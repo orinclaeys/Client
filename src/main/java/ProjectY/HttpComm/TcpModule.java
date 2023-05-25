bbeb9394b601dcfdae6d16f18a79cef9ce9f0fd9
orin_ <s0201603@ad.ua.ac.be>
2023-05-25 11:40:02 +0200

    Replication Test

--- src/main/java/ProjectY/HttpComm/TcpModule.java
+++ src/main/java/ProjectY/HttpComm/TcpModule.java
@@ -20,43 +20,43 @@ public class TcpModule {
     }
 
     public void sendFile(String ownerIP,String ReplicatorIP, String filename){
-        try {
-            System.out.println("TCP: " + ownerIP + " Filename: " + filename);
+        if (ReplicatorIP!=null) {
+            try {
+                System.out.println("TCP: " + ownerIP + " Filename: " + filename);
 
-            HttpModule httpModule = new HttpModule();
-            int portNumber = 5005;
-            JSONObject message = new JSONObject();
-            message.put("DestinationAddress",ReplicatorIP);
-            message.put("PortNumber",portNumber);
-            message.put("Filename", filename);
-            httpModule.sendFileInformation(ownerIP,message);
+                HttpModule httpModule = new HttpModule();
+                int portNumber = 5005;
+                JSONObject message = new JSONObject();
+                message.put("DestinationAddress", ReplicatorIP);
+                message.put("PortNumber", portNumber);
+                message.put("Filename", filename);
+                httpModule.sendFileInformation(ownerIP, message);
 
-            //Socket socket = new Socket(destinationIP,portNumber);
-            Socket socket = new Socket(ClientApplication.client.getIPAddres(), portNumber);
-            System.out.println("CLIENT: connected to server. Sending file...");
+                //Socket socket = new Socket(destinationIP,portNumber);
+                Socket socket = new Socket(ClientApplication.client.getIPAddres(), portNumber);
+                System.out.println("CLIENT: connected to server. Sending file...");
 
-            OutputStream outputStream = socket.getOutputStream();
+                OutputStream outputStream = socket.getOutputStream();
 
-            FileInputStream fileInputStream = new FileInputStream("src/main/java/ProjectY/Client/Files/" + filename);
+                FileInputStream fileInputStream = new FileInputStream("src/main/java/ProjectY/Client/Files/" + filename);
 
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = fileInputStream.read(buffer)) != -1){
-                outputStream.write(buffer,0,bytesRead);
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }
+
+                System.out.println("CLIENT: File sent");
+
+                fileInputStream.close();
+                outputStream.close();
+                socket.close();
+
+
+            } catch (Exception e) {
+                System.out.println(e.toString());
             }
-
-            System.out.println("CLIENT: File sent");
-
-            fileInputStream.close();
-            outputStream.close();
-            socket.close();
-
-
-
-        }catch (Exception e){
-            System.out.println(e.toString());
         }
-
    }
 
    public void receiveFile(int portNumber, String filename){
